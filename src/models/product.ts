c4681a16a4589cff64423d0e4b1bcb161ecc7d58
thatsddr <domenicodiruocco01@gmail.com>
2021-03-03 12:48:53 +0100

    added types

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,36 +1,46 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: "No name",
-        min: 2,
-        max: 50,
-    },
-    brand: {
-        type: String,
-        required: "No Brand error message",
-        min: 2,
-        max: 50,
-    },
-    corporation: {
-        type: String,
-        required: "No Corporation error message",
-        min: 2,
-        max: 50,
-    },
-    barcode: {
-        type: String,
-        required: "No Barcode error message",
-        min: 4,
-        max: 18,
-    },
-    state: {
-        type: String,
-        required: "No state",
-        min: 3,
-        max: 20,
-    },
-})
+interface ProductSchemaType extends mongoose.Document {
+	name: String;
+	brand: String;
+	corporation: String;
+	barcode: String;
+	state: String;
+}
 
-export default mongoose.model("Product", productSchema);
+const productSchema: mongoose.Schema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: "No name",
+		min: 2,
+		max: 50
+	},
+	brand: {
+		type: String,
+		required: "No Brand error message",
+		min: 2,
+		max: 50
+	},
+	corporation: {
+		type: String,
+		required: "No Corporation error message",
+		min: 2,
+		max: 50
+	},
+	barcode: {
+		type: String,
+		required: "No Barcode error message",
+		min: 4,
+		max: 18
+	},
+	state: {
+		type: String,
+		required: "No state",
+		min: 3,
+		max: 20
+	}
+});
+
+const Product: mongoose.Model<ProductSchemaType> = mongoose.model("Product", productSchema);
+export { productSchema };
+export default Product;
