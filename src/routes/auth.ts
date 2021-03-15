4aa4eecef70bc967ee1ac41e4cc971d927ba2238
Darjusch <Darjusch.schrand@code.berlin>
2021-03-15 16:44:32 +0100

    Register endpoint

---
+++ src/routes/auth.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import { registerUser } from '../controllers/auth';
+const router = express.Router();
+
+router.post('/register', registerUser); 
+
+export default router;
