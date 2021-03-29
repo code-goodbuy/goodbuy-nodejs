dc242b0b1a64be44cb956f23373d7c11b0db6238
Darjusch <Darjusch.schrand@code.berlin>
2021-03-29 16:33:57 +0200

    revoking refresh token and some clean up

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -40,15 +40,17 @@ export const registerUser = (req: Request, res: Response) => {
 export const loginUser = (req: Request, res: Response) => {
     const email: string = req.body.email
     const password: string = req.body.password
-    const tokenVersion: number = req.body.tokenVersion
     const user = UserModel.findOne({ email: email })
         .then(user => {
             if (user === null) {
                 return res.status(409).json({ message: "User does not exist or password/email is wrong" })
             }
+            if(user.tokenVersion == null) {
+                return res.status(401).json({ message: "Missing Token Version"})
+            }
             bcrypt.compare(password, user.password, function (err: Error, result: Boolean) {
                 if (result) {
-                    res.cookie('jid', createRefreshToken(email, tokenVersion), 
+                    res.cookie('jid', createRefreshToken(email, user.tokenVersion), 
                     {
                         httpOnly: true,
                         path: '/refresh_token'
@@ -69,7 +71,7 @@ export const loginUser = (req: Request, res: Response) => {
         );
 }
 
-export const authenticateToken = (req: any, res: Response, next: NextFunction) => {
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (token == null) return res.status(401).json({ message: "JWT is missing, access denied"})
@@ -77,7 +79,7 @@ export const authenticateToken = (req: any, res: Response, next: NextFunction) =
     const jwt = require('jsonwebtoken');
     jwt.verify(token, accessTokenSecret, (err: Error, user: User) => {
         if (err) return res.status(401).json({ err })
-        req.user = user
+        req.body.user = user
         next()
     })
 }
@@ -101,7 +103,7 @@ export const authenticateRefreshToken = (req: Request, res: Response, next: Next
 
 export const createAccessToken = (email: string) => {
     const accessTokenSecret: string = config.get("ACCESS_TOKEN_SECRET")
-    return sign({email: email}, accessTokenSecret, { expiresIn: '15m' })
+    return sign({email: email}, accessTokenSecret, { expiresIn: '5m' })
 }
 
 
@@ -111,9 +113,28 @@ export const createRefreshToken = (email: string, tokenVersion: number) => {
 }
 
 export const revokeRefreshToken = (req: Request, res: Response) => {
-    let tokenVersion: number = req.body.tokenVersion
-    tokenVersion += 1
-    return res.status(200).json({
-        message: "Revoked refresh_token successfully"
-    })
+    let newTokenVersion: number = req.body.tokenVersion
+    let email: string = req.body.email
+    if (email == null) {
+        return res.status(401).json({ message: "Missing Email"})
+    }
+    if (newTokenVersion == null) {
+        return res.status(401).json({ message: "Missing Token Version"})
+     }
+    newTokenVersion += 1
+    try{
+        let updatedUser = UserModel.updateOne(
+            {email: email}, 
+            {tokenVersion: newTokenVersion}
+            )
+        return res.status(200).json({
+            message: "Revoked refresh_token successfully"
+        })
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({
+            message: "Problem with revoking the token"
+        })
+    }
 }
