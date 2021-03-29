562885cab56532bb336471da17024ccf218ec6a0
Darjusch <Darjusch.schrand@code.berlin>
2021-03-29 16:32:43 +0200

    user has to be logged in to logout

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import { authenticateRefreshToken, loginUser, registerUser, revokeRefreshToken } from '../controllers/auth';
+import { authenticateRefreshToken, authenticateToken, loginUser, registerUser, revokeRefreshToken } from '../controllers/auth';
 const router = express.Router();
 
 router.post('/register', registerUser); 
 router.post('/login', loginUser);
 router.post('/refresh_token', authenticateRefreshToken);
-router.post('/revoke_refresh_token', revokeRefreshToken);
+router.post('/logout', authenticateToken, revokeRefreshToken);
 export default router;
