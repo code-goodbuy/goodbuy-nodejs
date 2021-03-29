eaa7d55aa7941f2e9e498df987b03f211caad5c2
Darjusch <Darjusch.schrand@code.berlin>
2021-03-29 11:45:59 +0200

    fixed wrong function call

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import { authenticateToken, loginUser, registerUser, revokeRefreshToken } from '../controllers/auth';
+import { authenticateRefreshToken, loginUser, registerUser, revokeRefreshToken } from '../controllers/auth';
 const router = express.Router();
 
 router.post('/register', registerUser); 
 router.post('/login', loginUser);
-router.post('/refresh_token', authenticateToken);
+router.post('/refresh_token', authenticateRefreshToken);
 router.post('/revoke_refresh_token', revokeRefreshToken);
 export default router;
