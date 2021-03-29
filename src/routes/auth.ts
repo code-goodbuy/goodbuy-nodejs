4effe201bc03be440114254963a0b2f0ad158e27
Darjusch <Darjusch.schrand@code.berlin>
2021-03-29 10:56:53 +0200

    revoke refresh token functionality

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { authenticateToken, loginUser, registerUser } from '../controllers/auth';
+import { authenticateToken, loginUser, registerUser, revokeRefreshToken } from '../controllers/auth';
 const router = express.Router();
 
 router.post('/register', registerUser); 
 router.post('/login', loginUser);
 router.post('/refresh_token', authenticateToken);
+router.post('/revoke_refresh_token', revokeRefreshToken);
 export default router;
