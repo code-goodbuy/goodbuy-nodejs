95854e09498504e24a53fa0142a6b73158674455
Darjusch <Darjusch.schrand@code.berlin>
2021-03-29 10:50:59 +0200

    added authentication for refresh token

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/auth';
+import { authenticateToken, loginUser, registerUser } from '../controllers/auth';
 const router = express.Router();
 
 router.post('/register', registerUser); 
 router.post('/login', loginUser);
+router.post('/refresh_token', authenticateToken);
 export default router;
