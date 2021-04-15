77368e9913a0b4615e6d9ceca63dd3d839a6a9cc
Darjusch <Darjusch.schrand@code.berlin>
2021-04-15 09:45:44 +0200

    User and product validation

--- src/validator/auth.ts
+++ src/validator/auth.ts
@@ -1,24 +1,45 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const createUserValidator = (req: Request , res: Response, next: NextFunction) => {
-    req.check('username', "Username is missing").notEmpty()
-    req.check('username', "Username must be between 6 to 22 Characters").isLength({
-        min:6, 
+    req.check('username', "Username is invalid").notEmpty().isAlphanumeric()
+    req.check('username', "Username must be between 5 to 22 Characters").isLength({
+        min:5, 
         max:22,
     });
-    req.check('email', "Email is missing").notEmpty()
+    req.check('email', "Email is invalid").notEmpty().isEmail().normalizeEmail()
     req.check('email', "Email must be between 6 to 40 Characters").isLength({
         min:6, 
         max:40,
     });
-    req.check('password', "Password is missing").notEmpty()
+    req.check('password', "Password is invalid").notEmpty().isString()
     req.check('password', "Password must be between 8 to 50 Characters").isLength({
         min:8, 
         max:50,
     });
-    req.check('acceptedTerms', "acceptedTerms is missing").notEmpty()
-    req.check('hasRequiredAge', "hasRequiredAge is missing").notEmpty()
-    req.check('tokenVersion', 'tokenVersion is missing').notEmpty()
+    req.check('acceptedTerms', "acceptedTerms is missing").notEmpty().isBoolean()
+    req.check('hasRequiredAge', "hasRequiredAge is missing").notEmpty().isBoolean()
+    req.check('tokenVersion', 'tokenVersion is missing').notEmpty().isNumeric()
+    // check for errors
+    const errors = req.validationErrors();
+    // if error occur show the first one as they happen
+    if (errors) {
+        const firstError = errors.map((error: { msg: string; }) => error.msg)[0];
+        return res.status(400).json({ error: firstError });
+    }
+    // proceed to next middleware
+    next();
+}
+export const loginUserValidator = (req: Request , res: Response, next: NextFunction) => {
+    req.check('email', "Email is invalid").notEmpty().isEmail().normalizeEmail()
+    req.check('email', "Email must be between 6 to 40 Characters").isLength({
+        min:6, 
+        max:40,
+    });
+    req.check('password', "Password is invalid").notEmpty().isString()
+    req.check('password', "Password must be between 8 to 50 Characters").isLength({
+        min:8, 
+        max:50,
+    });
     // check for errors
     const errors = req.validationErrors();
     // if error occur show the first one as they happen
--- src/validator/product.ts
+++ src/validator/product.ts
@@ -1,27 +1,27 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const createProductValidator = (req: Request , res: Response, next: NextFunction) => {
-    req.check('name', "Product name is missing").notEmpty()
+    req.check('name', "Product name is missing").notEmpty().isAlpha()
     req.check('name', "Product name must be between 2 to 50 Characters").isLength({
         min:2, 
         max:50,
     });
-    req.check('brand', "Brand name is missing").notEmpty()
+    req.check('brand', "Brand name is missing").notEmpty().isAlpha()
     req.check('brand', "Brand name must be between 2 to 50 Characters").isLength({
         min:2, 
         max:50,
     });
-    req.check('corporation', "Corporation name is missing").notEmpty()
+    req.check('corporation', "Corporation name is missing").notEmpty().isAlpha()
     req.check('corporation', "Corporation name must be between 2 to 50 Characters").isLength({
         min:2, 
         max:50,
     });
-    req.check('barcode', "Barcode is missing").notEmpty()
+    req.check('barcode', "Barcode is invalid").notEmpty().isString()
     req.check('barcode', "Barcode must be between 4 to 18 Characters").isLength({
         min:4, 
         max:18,
     });
-    req.check('state', "Product state is missing").notEmpty()
+    req.check('state', "Product state is missing").notEmpty().isAlpha()
     req.check('state', "Product state must be between 3 and 20 Characters").isLength({
         min:3, 
         max:20,
