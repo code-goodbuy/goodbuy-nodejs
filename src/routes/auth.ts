13a10d91b16fc33900c2fecc3cc2440a17060f4c
Darjusch <Darjusch.schrand@code.berlin>
2021-04-16 15:29:57 +0200

    new route that receives the confirmation code

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { authenticateRefreshToken, authenticateToken, loginUser, registerUser, revokeRefreshToken } from '../controllers/auth';
+import { confirmUser } from '../controllers/email';
 const router = express.Router();
 
 router.post('/register', registerUser); 
 router.post('/login', loginUser);
 router.post('/refresh_token', authenticateRefreshToken);
 router.post('/logout', authenticateToken, revokeRefreshToken);
+router.get('/confirm_email/:confirmationCode', confirmUser);
 export default router;
