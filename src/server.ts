8e02a211047bbd0c5ddbcbf244303504fe4fac5b
David P <david.pettersson@code.berlin>
2021-04-21 14:21:17 +0200

    - split the app into server.ts and app.ts to clean up the structure

--- src/server.ts
+++ src/server.ts
@@ -1,75 +1,83 @@
-import express from "express";
-const app = express();
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import morgan from "morgan";
-import expressValidator from "express-validator";
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import promBundle from "express-prom-bundle";
-const metricsMiddleware = promBundle({
-  autoregister: true,
-  includeStatusCode: true,
-  includePath: true,
-  includeMethod: true,
-  includeUp: true,
-});
-import job from "./utils/db_cronjob";
-import rt from "file-stream-rotator";
+// import express from "express";
+// const app = express();
+// import mongoose from "mongoose";
+// import bodyParser from "body-parser";
+// import morgan from "morgan";
+// import expressValidator from "express-validator";
+// import cors from 'cors';
+// import cookieParser from 'cookie-parser';
+// import promBundle from "express-prom-bundle";
+// const metricsMiddleware = promBundle({
+//   autoregister: true,
+//   includeStatusCode: true,
+//   includePath: true,
+//   includeMethod: true,
+//   includeUp: true,
+// });
+// import job from "./utils/db_cronjob";
+// import rt from "file-stream-rotator";
+//
+// // log formatting
+// morgan.token(
+//   "custom",
+//   // @ts-ignore
+//   "[:date[iso]] [:user-agent] [:http-version] [:method] [:url] [:status] [:total-time ms]"
+// );
+//
+// // writing log stream
+// let accessLogStream = rt.getStream({
+//   filename: "log/access-%DATE%.log",
+//   frequency: "daily",
+//   verbose: true,
+// });
+//
+// let db = "";
+//
+// if (process.env.DBHost) {
+//   db = process.env.DBHost;
+// }
+//
+// mongoose
+//   .connect(db, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//   })
+//   .then(async (connection) => {
+//     app.listen(app.get("port"), () => {
+//       console.log("Database connected!");
+//       job.backupDB();
+//     });
+//     app.on("close", () => {
+//       app.removeAllListeners();
+//     });
+//   })
+//   .catch((err) => console.log(err));
+//
+// // bring in routes
+// import productRoutes from "./routes/product";
+// import authRoutes from "./routes/auth";
+//
+// //middleware
+// app.use(cors());
+// app.use(cookieParser());
+// app.use(morgan("dev" ,{ skip: (req, res) => process.env.NODE_ENV === 'test' })); // Logging HTTP Requests and Errors
+// app.use(morgan("custom", { stream: accessLogStream })); // writing log stream in 'log/access'
+// app.use(bodyParser.json({limit: 1000, type: "application/json"})); // The size limit of request in bytes + content type
+// app.use(expressValidator()); // Validate incoming data
+// app.use(metricsMiddleware); // Prometheus logging
+// app.use("/", authRoutes);
+// app.use("/", productRoutes);
+//
+// const port = 8080;
+// module.exports = app.listen(port, () => {
+//   console.log(`Your Node Js API is listening on port: ${port}`);
+// });
 
-// log formatting
-morgan.token(
-  "custom",
-  // @ts-ignore
-  "[:date[iso]] [:user-agent] [:http-version] [:method] [:url] [:status] [:total-time ms]"
-);
+import App from './app'
 
-// writing log stream
-let accessLogStream = rt.getStream({
-  filename: "log/access-%DATE%.log",
-  frequency: "daily",
-  verbose: true,
-});
+const app = new App();
 
-let db = "";
+app.listen();
 
-if (process.env.DBHost) {
-  db = process.env.DBHost;
-}
-
-mongoose
-  .connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(async (connection) => {
-    app.listen(app.get("port"), () => {
-      console.log("Database connected!");
-      job.backupDB();
-    });
-    app.on("close", () => {
-      app.removeAllListeners();
-    });
-  })
-  .catch((err) => console.log(err));
-
-// bring in routes
-import productRoutes from "./routes/product";
-import authRoutes from "./routes/auth";
-
-//middleware
-app.use(cors());
-app.use(cookieParser());
-app.use(morgan("dev" ,{ skip: (req, res) => process.env.NODE_ENV === 'test' })); // Logging HTTP Requests and Errors
-app.use(morgan("custom", { stream: accessLogStream })); // writing log stream in 'log/access'
-app.use(bodyParser.json({limit: 1000, type: "application/json"})); // The size limit of request in bytes + content type 
-app.use(expressValidator()); // Validate incoming data
-app.use(metricsMiddleware); // Prometheus logging
-app.use("/", authRoutes);
-app.use("/", productRoutes);
-
-const port = 8080;
-module.exports = app.listen(port, () => {
-  console.log(`Your Node Js API is listening on port: ${port}`);
-});
+module.exports = app.app;
