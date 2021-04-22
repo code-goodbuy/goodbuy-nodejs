937ee2358b15b726d7ebc2535ea8968ce45b8da0
jwdotpark <jongwoo.park@code.berlin>
2021-04-23 01:54:13 +0200

    profile routes init

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-// import { authenticateRefreshToken, authenticateToken, loginUser, registerUser, revokeRefreshToken } from '../controllers/auth';
+import { updateProfile } from "../controllers/profile";
+import { authenticateRefreshToken, authenticateToken, loginUser, registerUser, revokeRefreshToken } from '../controllers/auth';
 // import { confirmUser } from '../controllers/email';
 // import { createUserValidator, loginUserValidator } from '../validator/auth';
 const router = express.Router();
@@ -9,5 +10,6 @@ const router = express.Router();
 // router.post('/refresh_token', authenticateRefreshToken);
 // router.post('/logout', authenticateToken, revokeRefreshToken);
 // router.post('/confirm_email/:confirmationCode', confirmUser);
+router.post('/profile', updateProfile)
 
 export default router;
