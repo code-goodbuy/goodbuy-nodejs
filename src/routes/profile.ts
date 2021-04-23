fc90098d91a7dae999b2d100939414ad083d7268
jwdotpark <jongwoo.park@code.berlin>
2021-04-23 02:04:55 +0200

    deleted unused dependencies

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,15 +1,8 @@
 import express from 'express';
 import { updateProfile } from "../controllers/profile";
-import { authenticateRefreshToken, authenticateToken, loginUser, registerUser, revokeRefreshToken } from '../controllers/auth';
-// import { confirmUser } from '../controllers/email';
-// import { createUserValidator, loginUserValidator } from '../validator/auth';
+import { authenticateToken } from '../controllers/auth';
 const router = express.Router();
 
-// router.post('/register', createUserValidator, registerUser); 
-// router.post('/login', loginUserValidator, loginUser);
-// router.post('/refresh_token', authenticateRefreshToken);
-// router.post('/logout', authenticateToken, revokeRefreshToken);
-// router.post('/confirm_email/:confirmationCode', confirmUser);
-router.post('/profile', updateProfile)
+router.post('/profile', authenticateToken, updateProfile)
 
 export default router;
