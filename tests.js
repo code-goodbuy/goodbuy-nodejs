fde6a1da420bf48d4141786e728b7953ac7d9f59
jwdotpark <jongwoo.park@code.berlin>
2021-04-23 23:38:14 +0200

    cleanup temporarily disabled

--- tests.js
+++ tests.js
@@ -1,9 +1,11 @@
 require("./test/auth");
-require("./test/profile");
 require("./test/rabbitmq");
-require("./test/product");
 require("./test/dbBackup.test");
+require("./test/product");
 require("./test/cleanup");
+
+// need user info in the db
+require("./test/profile");
 
 // Add the test files in the order you want them executed.
 // Leave cleanup as last requirement
