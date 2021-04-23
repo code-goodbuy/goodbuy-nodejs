0f5e534408b9e9ce65195b1dd7183ab02c71c03c
Darjusch <Darjusch.schrand@code.berlin>
2021-04-23 13:43:36 +0200

    added /api to tests

--- test/rabbitmq.js
+++ test/rabbitmq.js
@@ -10,7 +10,7 @@ describe('Product not in DB', () => {
         it('returns 204, the product is not in the db, it starts the scraper', (done) => {
             let requested_ean = { ean: "1234567890123" }
             chai.request(server)
-                .get('/product/' + requested_ean.ean)
+                .get('/api/product/' + requested_ean.ean)
                 .send(requested_ean)
                 .end((err, res) => {
                     res.should.have.status(204);
