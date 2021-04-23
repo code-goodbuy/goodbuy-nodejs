2d3ca3e256bc78924be428a7e81c332bc7d5d803
jwdotpark <jongwoo.park@code.berlin>
2021-04-23 23:08:01 +0200

    added profile test

--- tests.js
+++ tests.js
@@ -1,8 +1,9 @@
-require('./test/auth');
-require('./test/rabbitmq');
-require('./test/product');
-require('./test/dbBackup.test');
-require('./test/cleanup');
+require("./test/auth");
+require("./test/profile");
+require("./test/rabbitmq");
+require("./test/product");
+require("./test/dbBackup.test");
+require("./test/cleanup");
 
 // Add the test files in the order you want them executed.
 // Leave cleanup as last requirement
