915974eff7b195064f891e4c2425d4432c225e04
jwdotpark <jongwoo.park@code.berlin>
2021-04-23 23:40:35 +0200

    profile test order temporarily changed

--- tests.js
+++ tests.js
@@ -3,8 +3,7 @@ require("./test/rabbitmq");
 require("./test/dbBackup.test");
 require("./test/product");
 require("./test/cleanup");
-
-// need user info in the db
+// FIXME figure out why profile test should be the last for testing pass :/
 require("./test/profile");
 
 // Add the test files in the order you want them executed.
