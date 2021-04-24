2c7ef2831cc7c2fe19503e33e90c2a9c6120de37
jwdotpark <jongwoo.park@code.berlin>
2021-04-25 00:08:34 +0200

    fixed: getProfile response order

--- src/controllers/profile.ts
+++ src/controllers/profile.ts
@@ -1,30 +1,26 @@
-import User from "../models/user.model";
+import UserModel from "../models/user.model";
 import { Request, Response, NextFunction } from "express";
 
 // TODO add api doc to swagger
-// TODO better statuscode for error handling
-// NOTE should profile page have param route?
 
 export const getProfile = (req: Request, res: Response, next: NextFunction) => {
   const { email } = req.body;
-  User.find({ email: email }, function (err, results) {
-    return res
-      .status(200)
-      .send({ results: results });
+  UserModel.findOne({ email: email }, function (err: Error, results: any) {
+    if (results) {
+      return res.status(200).send({
+        _id: results._id,
+        username: results.username,
+        email: results.email,
+        description: results.description,
+        imageURL: results.imageURL,
+      });
+    } else {
+      return res.status(500).json({ message: "internal error" });
+    }
   })
-    .select("username email description imageURL")
-    .then((success) => {
-      if (success) {
-      } else {
-        return res.status(500).json({
-          message: "internal error",
-        });
-      }
-    })
     .catch((err: Error) => {
       console.log(err);
     });
-  next();
 };
 
 export const updateProfile = (
@@ -33,11 +29,11 @@ export const updateProfile = (
   next: NextFunction
 ) => {
   const { email, description, imageURL } = req.body;
-  const user = new User({
+  const user = new UserModel({
     description: description,
     imageURL: imageURL,
   });
-  User.findOneAndUpdate(
+  UserModel.findOneAndUpdate(
     // filter, update, option
     { email: email },
     { description: description, imageURL: imageURL },
