604a3c4f11b97cf3ab0d32bef33851213e900cea
jwdotpark <jwdotpark@me.com>
2021-04-27 16:05:13 +0200

    removed comment

--- test/profile.js
+++ test/profile.js
@@ -33,8 +33,8 @@ describe("Authorization", () => {
           res.should.have.status(200);
           res.body.should.have.property("_id")
           res.body.should.have.property("username")
-          res.body.should.have.property("email")    
-      })
+          res.body.should.have.property("email")
+        })
       done();
     })
     it("should fail to get profile result", (done) => {
@@ -44,7 +44,7 @@ describe("Authorization", () => {
         .send(userInfo.email)
         .end((err, res) => {
           res.should.have.status(401);
-      })
+        })
       done();
     })
   })
@@ -57,7 +57,6 @@ describe("Authorization", () => {
         .set({ Authorization: `Bearer ${accessToken}` })
         .send(userInfo)
         .end((err, res) => {
-          console.log(res)
           res.should.have.status(200);
           res.body.should.have.property("_id")
           res.body.should.have.property("username")
@@ -79,96 +78,3 @@ describe("Authorization", () => {
     })
   })
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// describe("Authorization", () => {
-//   const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
-  
-//   const accessToken = jwt.sign(
-//     { email: "testmail12345@test.de" },
-//     accessTokenSecret,
-//     { expiresIn: "5m" }
-//   );
-  
-//   const userInfo = {
-//     email: "testmail12345@test.de",
-//     imageURL: "https://picsum.photos/1200/1400.jpg",
-//     description: "some description"
-//   }
-
-// describe("/GET profile", () => {
-//   it("should get user profile result", (done) => {
-//     chai
-//       .request(server)
-//       .get('/api/profile')
-//       .set({ Authorization: `Bearer ${accessToken}` })
-//       .send(userInfo.email)
-//       .end((err, res) => {
-//         res.should.have.status(200);
-//         res.body.product.should.have.property("_id");
-//         res.body.product.should.have.property("username");
-//         res.body.product.should.have.property("email");
-//         done();
-
-//       });
-//   });
-  
-//   it("should fail without token", (done) => {
-//     chai
-//       .request(server)
-//       .get("/api/profile")
-//       .send(userInfo.email)
-//       .end((err, res) => {
-//         res.should.have.status(409);
-//       });
-//     done();
-//   });
-// });
-
-// describe("/POST user information", () => {
-//   it('should post user profile with token', (done) => {
-//     chai
-//       .request(server)
-//       .put('/api/profile')
-//       .set({ Authorization: `Bearer ${accessToken}` })
-//       .send(userInfo)
-//       .end((err, res) => {
-//         res.should.have.status(200);
-//         res.body.product.should.have.property("_id");
-//         res.body.product.should.have.property("username");
-//         res.body.product.should.have.property("description");
-//         res.body.product.should.have.property("imageURL");
-//       })
-//     done();
-//   })
-
-//   it("should fail to post user info without token ", (done) => {
-//     chai
-//       .request(server)
-//       .put("/api/profile")
-//       .send(userInfo)
-//       .end((err, res) => {
-//         res.should.have.status(409);
-//       });
-//     done();
-//   });
-// });
-// });
