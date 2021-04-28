3e5102a6f27764808a85f2d30b0aaa2a38278ca6
Darjusch <Darjusch.schrand@code.berlin>
2021-04-28 15:04:08 +0200

    returns only verified products

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -5,7 +5,7 @@ import { sendEanToRabbitMQ } from "../utils/send_ean";
 // Todo review all inputs and validate them same as in auth.ts 
 // Create some kind of product and user validator that acts as a middleware
 export const getAllProducts = (req: Request, res: Response) => {
-    const products = ProductModel.find().limit(5)
+    const products = ProductModel.find({state: "verified"}).limit(5)
     .select("name brand corporation ean state")
     .then(products => {
         return res.status(200).json({products: products})
