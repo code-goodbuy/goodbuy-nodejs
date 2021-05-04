d3ad7622df5328b430acd65a54f4fbd59bd359e6
jwdotpark <jongwoo.park@code.berlin>
2021-05-04 16:43:16 +0200

    added get profile with param route

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import { getProfile, updateProfile } from "../controllers/profile";
+import { getProfile, updateProfile, getOtherProfile } from "../controllers/profile";
 import { authenticateToken } from '../controllers/auth';
 import { updateProfileValidator } from '../validator/profile';
 const router = express.Router();
 
+// own profile
 router.get('/profile', authenticateToken, getProfile)
 router.put('/profile', updateProfileValidator, authenticateToken, updateProfile)
+// other profile
+router.get('/profile/:username', getOtherProfile)
 
 export default router;
