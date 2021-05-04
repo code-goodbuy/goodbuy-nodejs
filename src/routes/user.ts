6fddf5df734e9afa502f630abb794a34580029de
jwdotpark <jongwoo.park@code.berlin>
2021-05-04 17:35:03 +0200

    removed unused deps

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,5 @@
 import express from 'express';
 import { getOtherProfile } from "../controllers/profile";
-// import { authenticateToken } from '../controllers/auth';
-// import { updateProfileValidator } from '../validator/profile';
 const router = express.Router();
 
 router.get('/user/:username', getOtherProfile)
