39b86f0afd72969ed4e8ff5784082a309cd234d3
jwdotpark <jongwoo.park@code.berlin>
2021-05-09 14:33:37 +0200

    dead code removed

--- test/profile.js
+++ test/profile.js
@@ -6,8 +6,6 @@ let chai = require("chai");
 let chaiHttp = require("chai-http");
 let chaiCookie = require("chai-expected-cookie");
 let server = require("../dist/server");
-// let expect = chai.expect;
-// let jwt = require("jsonwebtoken");
 chai.use(chaiHttp);
 chai.use(chaiCookie);
 
